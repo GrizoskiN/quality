4679c6838da4f624c58b54809494f497443c9f52
GrizoskiN <ngrizo@gmail.com>
2024-02-22 19:51:25 +0100

    dd

--- src/pages/sitemap.jsx
+++
@@ -1,8 +0,0 @@
-export default async function SitemapXml(req, res) {
-    const yoastSitemapUrl = 'https://blog.gobro.studio/sitemap_index.xml'; 
-    const response = await fetch(yoastSitemapUrl);
-    const sitemapXml = await response.text();
-  
-    res.setHeader('Content-Type', 'text/xml');
-    res.status(200).send(sitemapXml);
-  }
